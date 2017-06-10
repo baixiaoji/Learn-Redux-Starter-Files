5f77a698349f03be2e8e99eebf6e4cbd069bcaaf
Kevin <1041874421@qq.com>
2017-06-10 10:47:43 +0800

    使用react-router 1.看看阮一峰的文章 2. 明白了参数的意思

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,10 +1,25 @@
 // let's go!
 import React from "react"
 
-import {render} from 'react-dom'
+import { render } from 'react-dom'
 
 import './styles/style.styl'
 
 // import component
 import Main from "./components/Main"
-render(<Main />,document.getElementById("root"))
+import PhotoGrid from "./components/PhotoGrid"
+import Single from "./components/Single"
+
+// import router
+import { Router, Route, IndexRoute, hashHistory } from "react-router"
+
+const routes = (
+    <Router history={hashHistory}>
+        <Route path="/" component={Main}>
+            <IndexRoute component={PhotoGrid}></IndexRoute>
+            <Route path="view/:Postid" component={Single}></Route>
+        </Route>
+    </Router>
+)
+
+render(routes, document.getElementById("root"))

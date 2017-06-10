140a9f7ca184990e7f1132856c6529c5b82e063c
Kevin <1041874421@qq.com>
2017-06-10 14:45:13 +0800

    update the state width reducers 因为每一次的dispatch都会调用全部的reducers，所以要有在里面的判断， 而且reducers是纯函数，是不能对原来的的state进行修改，所以要有新的状态返回

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Photo from "./Photo"
+
 
 class PhotoGrid extends Component {
     render() {
         return (
             <div className="photo-grid">
-               I'm photo-grid
+                {this.props.posts.map((post, i) => {
+                    return <Photo {...this.props} post={post} key={i} i={i}/>
+                })}
             </div>
         )
     }
--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -3,9 +3,22 @@
  * @param {*} state  current state 
  * @param {*} action 触发的事件（action）
  */
-function posts(state = [], action){
-    console.log(state, action)
-    return state;
+function posts(state = [], action) {
+    switch (action.type) {
+        case "INCREMENT":
+            // const newState = Object.assign({}, state)
+            const i = action.index;
+            // newState[i].likes ++;
+            // console.log("758",newState)
+            // return [...newState]
+            return [
+                ...state.slice(0, i),
+                { ...state[i], likes: state[i].likes + 1 },
+                ...state.slice(i + 1)
+            ]
+        default:
+            return state;
+    }
 }
 
 export default posts;

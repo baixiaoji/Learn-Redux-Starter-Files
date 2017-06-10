a0c2a8f956dc9436be77e9ac3d32d7ba8b688af5
Kevin <1041874421@qq.com>
2017-06-10 10:04:51 +0800

    新建组件：

---
+++ learn-redux/client/components/Main.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+// const  Main = React.createClass({
+//     render() {
+//         return (
+//             <div>
+//                 <h1>
+//                     Hi
+//                </h1>
+//             </div>
+//         )
+//     }
+// })
+// React.createClass 与 ES6 语法糖的区别
+// https://toddmotto.com/react-create-class-versus-component/ 
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Hi
+               </h1>
+            </div>
+        )
+    }
+}
+
+export default Main
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,10 @@
 // let's go!
+import React from "react"
+
+import {render} from 'react-dom'
+
+import './styles/style.styl'
+
+// import component
+import Main from "./components/Main"
+render(<Main />,document.getElementById("root"))
